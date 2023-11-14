bd02ab02e144d283a9353ad73e3ae0639ff13b2b
Mari <52796151+mary97mr@users.noreply.github.com>
2023-11-14 08:30:52 +0100

    add validation in server actions, use hook useFormState, improve accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,24 +6,45 @@ import { redirect } from 'next/navigation';
 
 const InvoiceSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({invalid_type_error: "Select a valid customer"}),
+  amount: z.coerce.number().gt(0, 'Please, enter an amount grater than 0$'),
+  status: z.enum(['pending', 'paid'], {invalid_type_error: 'Please, select an invoice status'}),
   date: z.string()
 })
 
 const CreateInvoiceSchema = InvoiceSchema.omit({id: true, date: true})
 const UpdateInvoiceSchema  = InvoiceSchema.omit({id: true, date: true})
 
-export async function createInvoice(formData: FormData) {
-  const {customerId, amount, status} = CreateInvoiceSchema.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoiceSchema.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing fields failed to create invoice"
+    }
+  }
   const urlInvoices = '/dashboard/invoices'
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
+
+  // Insert data into the database
   try {
     await sql `
       INSERT INTO invoices (customer_id, amount, status, date)
@@ -34,17 +55,27 @@ export async function createInvoice(formData: FormData) {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
+
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath(urlInvoices)
   redirect(urlInvoices)
 }
 
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const {customerId, amount, status} = UpdateInvoiceSchema.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoiceSchema.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Failed to update invoice"
+    }
+  }
+  const {customerId, amount, status} = validatedFields.data
   const urlInvoices = '/dashboard/invoices'
   const amountInCents = amount * 100
   try {
@@ -63,7 +94,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Something wrong happened')
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
